f8a81316d0132dc45ea1594225a81d5206aec830
Arco <yiyezhihen@gmail.com>
2019-06-21 15:32:44 +0800

    Finish chapter 10

--- Sources/App/Controllers/AcronymsController.swift
+++ Sources/App/Controllers/AcronymsController.swift
@@ -15,6 +15,9 @@ final class AcronymsController: RouteCollection {
     acronymsRoutes.get("first", use: firstHandler)
     acronymsRoutes.get("sorted", use: sortedHandler)
     acronymsRoutes.get(Acronym.parameter, "user", use: getUserHandler)
+    acronymsRoutes.post(Acronym.parameter, "categories", Category.parameter, use: addCategoriesHandler)
+    acronymsRoutes.get(Acronym.parameter, "categories", use: getCategoriesHandler)
+    acronymsRoutes.delete(Acronym.parameter, "categories", Category.parameter, use: removeCategoriesHandler)
   }
   
   func getAllHandler(_ req: Request) throws -> Future<[Acronym]> {
@@ -72,6 +75,26 @@ final class AcronymsController: RouteCollection {
   func getUserHandler(_ req: Request) throws -> Future<User> {
     return try req.parameters.next(Acronym.self)
       .flatMap { $0.user.get(on: req) }
+  }
+  
+  
+  func addCategoriesHandler(_ req: Request) throws -> Future<HTTPStatus> {
+    return try flatMap(req.parameters.next(Acronym.self), req.parameters.next(Category.self)) { acronym, category in
+      return acronym.categories.attach(category, on: req)
+        .transform(to: .created)
+    }
+  }
+  
+  func getCategoriesHandler(_ req: Request) throws -> Future<[Category]> {
+    return try req.parameters.next(Acronym.self)
+      .flatMap { try $0.categories.query(on: req).all() }
+  }
+  
+  func removeCategoriesHandler(_ req: Request) throws -> Future<HTTPStatus> {
+    return try flatMap(req.parameters.next(Acronym.self), req.parameters.next(Category.self)) { acronym, category in
+      return acronym.categories.detach(category, on: req)
+        .transform(to: .noContent)
+    }
   }
   
 }
