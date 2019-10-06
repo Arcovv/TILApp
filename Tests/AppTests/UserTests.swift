0418576b83e2c4f2e8addce6ec076b60116288eb
Arco <yiyezhihen@gmail.com>
2019-10-06 23:56:23 +0800

    Finish tests

--- Tests/AppTests/UserTests.swift
+++ Tests/AppTests/UserTests.swift
@@ -34,4 +34,56 @@ final class UserTests: XCTestCase {
     XCTAssertEqual(users[0].id, user.id)
   }
   
+  func testUserCanBeSavedWithAPI() throws {
+    let user = User(name: usersName, username: usersUsername)
+    let receivedUser = try app.getResponse(
+      to: usersURI,
+      method: .POST,
+      headers: ["Content-Type": "application/json"],
+      data: user,
+      decodeTo: User.self
+    )
+    
+    XCTAssertEqual(receivedUser.name, user.name)
+    XCTAssertEqual(receivedUser.username, user.username)
+    XCTAssertNotNil(receivedUser.id)
+    
+    let users = try app.getResponse(to: usersURI, decodeTo: [User].self)
+    XCTAssertEqual(users.count, 1)
+    XCTAssertEqual(users[0].name, receivedUser.name)
+    XCTAssertEqual(users[0].username, receivedUser.username)
+    XCTAssertEqual(users[0].id, receivedUser.id)
+  }
+  
+  func testGettingASingleUserFromTheAPI() throws {
+    let user = try User.create(name: usersName, username: usersUsername, on: conn)
+    let receivedUser = try app.getResponse(to: "\(usersURI)/\(user.id!)", decodeTo: User.self)
+    XCTAssertEqual(user.name, receivedUser.name)
+    XCTAssertEqual(user.username, receivedUser.username)
+    XCTAssertEqual(user.id, receivedUser.id)
+  }
+  
+  func testGettingAUsersAcronymsFromTheAPI() throws {
+    let user = try User.create(on: conn)
+    
+    let short = "OMG"
+    let long = "Oh My God"
+    
+    let acronym1 = try Acronym.create(short: short, long: long, user: user, on: conn)
+    _ = try Acronym.create(short: "LOL", long: "Laugh Ont Loud", user: user, on: conn)
+    
+    let acronyms = try app.getResponse(to: "\(usersURI)/\(user.id!)/acronyms", decodeTo: [Acronym].self)
+    XCTAssertEqual(acronyms.count, 2)
+    XCTAssertEqual(acronyms[0].short, acronym1.short)
+    XCTAssertEqual(acronyms[0].long, acronym1.long)
+    XCTAssertEqual(acronyms[0].id, acronym1.id)
+  }
+  
+  static let allTests = [
+    ("testUsersCanBeRetrievedFromAPI", testUsersCanBeRetrievedFromAPI),
+    ("testUserCanBeSavedWithAPI", testUserCanBeSavedWithAPI),
+    ("testGettingASingleUserFromTheAPI", testGettingASingleUserFromTheAPI),
+    ("testGettingAUsersAcronymsFromTheAPI", testGettingAUsersAcronymsFromTheAPI)
+  ]
+  
 }
